91cc2c0aa1b9fa005649265a89471c2b27dbaae4
SpaceCem <turgutcemyazici@gmail.com>
2026-04-21 18:28:56 +0000

    Update kelime.js

--- games/001kelimelik/kelime.js
+++ games/001kelimelik/kelime.js
@@ -18,7 +18,7 @@ let rakipAdaylar = [];
 const kelimeSeti = new Set(SOZLUK.map(k => k.toUpperCase('tr-TR')));
 
 const harfPuanlari = {
-    'A':50,'E':1,'İ':1,'I':2,'O':2,'U':2,'Ö':4,'Ü':3,
+    'A':1,'E':1,'İ':1,'I':2,'O':2,'U':2,'Ö':4,'Ü':3,
     'K':1,'L':1,'R':1,'N':1,'T':1,'M':2,'S':2,'B':3,'D':3,
     'Y':3,'C':3,'Ç':4,'H':4,'P':4,'Ş':4,'G':4,'V':5,'Z':5,
     'Ğ':7,'J':10,'F':10
